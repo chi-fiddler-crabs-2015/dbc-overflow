fa7abf583dece30458f9c09342fb434c38da722c
David Vaynshteyn & kaylindenberg & Ty Kowalewski <pair+dvayns2.kaylindenberg.tykowale@devbootcamp.com>
2015-03-18 16:04:05 -0500

    model for controller, shits broken

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -2,5 +2,44 @@ require 'rails_helper'
 
 
 describe QuestionsController do
-  pending
+
+  context "GET #index" do
+    it 'assigns @questions to all questions' do
+      question = create(:question)
+      expect(assigns(:questions)).to eq(Question.all)
+    end
+  end
+
+  context "GET #new" do
+    it 'assigns question to @question' do
+    end
+  end
+
+  context "GET #show" do
+    it 'assigns the question to @question' do
+    end
+  end
+
+  context "POST #create" do
+
+    describe "when valid params are passed" do
+      it 'creates a new questions' do
+      end
+
+      it 'assigns a newly created question as a question' do
+      end
+
+      it 'redirects to the newly created question' do
+      end
+    end
+
+    describe "when invalid params are passed" do
+      it 'does not create a new question' do
+      end
+
+      it 'sends the user to new question page' do
+      end
+    end
+  end
+
 end
