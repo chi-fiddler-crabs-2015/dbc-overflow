a15e20ffe2a2ea2c84cb991957b9313bce8a5ed4
David Vaynshteyn & kaylindenberg & Ty Kowalewski <pair+dvayns2.kaylindenberg.tykowale@devbootcamp.com>
2015-03-18 14:57:26 -0500

    Skeleton for questions controller

---
+++ app/controllers/question_controller.rb
@@ -0,0 +1,20 @@
+class QuestionController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  def edit
+  end
+end
