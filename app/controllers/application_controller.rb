ef0f855c6386c2cb1f59c0c38b83b8e9031364c2
kaylindenberg <kay.lindenberg@gmail.com>
2015-03-18 22:42:44 -0500

    added additional questions controller updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
     @current_user ||= session[:user_id] && User.find_by(id: session[:user_id])
   end
 
+  def logged_in?
+	return true if current_user != nil
+  end
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
