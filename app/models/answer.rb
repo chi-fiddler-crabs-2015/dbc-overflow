93e270f9bdd41b5ba5def7bb75366578ceed9f15
David Vaynshteyn <dvaynshteyn@David-Vaynshteyns-MacBook-Pro.local>
2015-03-18 21:43:11 -0500

    added validations to the models

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,9 +4,17 @@ class Answer < ActiveRecord::Base
   has_many :comments, as: :response
   has_many :votes, as: :tally
 
-  validates :content, presence: true
+  validates :content, presence: true, uniqueness: true
+
+  validate :content_must_not_be_nil
 
   def vote_count
+  end
+
+  def content_must_not_be_nil
+    if self.content.length == 0
+      errors.add(:content, "You entered a blank answer")
+    end
   end
 
 end
