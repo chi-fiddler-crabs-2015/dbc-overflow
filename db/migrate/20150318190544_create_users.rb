5f9fc4294c894035cc9f2f710c08dde009123ecf
Ty Kowalewski <tykowale@gmail.com>
2015-03-19 16:10:34 -0500

    fixed users migrations

--- db/migrate/20150318190544_create_users.rb
+++ db/migrate/20150318190544_create_users.rb
@@ -7,6 +7,7 @@ class CreateUsers < ActiveRecord::Migration
 
       t.timestamps
     end
-    add_index :username, :email, unique: true
+    add_index :users, :email, unique: true
+    add_index :users, :username, unique: true
   end
 end
