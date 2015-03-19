b7c7d7b1a0da62d659c03cf58359c4d06b83e7d2
Ty Kowalewski <tykowale@gmail.com>
2015-03-19 09:44:45 -0500

    Started votes controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,16 @@
 class VotesController < ApplicationController
+
+  def new
+  end
+
+  def create
+
+
+    if @vote.save
+      redirect_to questions_path(vote.id)
+    else
+      render questions_path(vote.id), notice: "You vote was invalid"
+    end
+  end
+
 end
