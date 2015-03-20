df91aec552394353d1190bae15d323728c75b905
Ty Kowalewski <tykowale@gmail.com>
2015-03-20 12:38:20 -0500

    added ajax for answers, need to change css to append in a better spot

---
+++ app/assets/javascripts/add-answer.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.new-answer-form').on("ajax:complete", function(e, data) {
+    console.log(data.responseText)
+    $('.new-comment-form').last().prepend(data.responseText)
+    // comment_list = $(this).parent('.add-new-comment')
+    // comment_list.prepend(data.responseText)
+    // $("#comment_content").val('')
+  })
+})
--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -5,10 +5,18 @@ class AnswersController < ApplicationController
   end
 
   def create
-    question = Question.find_by(id: params[:question_id])
-    @answer = question.answers.new(answer_params)
+    puts params
+    @question = Question.find_by(id: params[:question_id])
+    @answer = @question.answers.new(answer_params)
     @answer.update_attributes(user: current_user)
     if @answer.save
+      respond_to do |format|
+        format.js do
+            render partial: 'add_ajax_answer' and return
+          end
+        end
+        format.any do
+        end
       redirect_to question_path(params[:question_id])
     else
       @errors = @answer.errors.full_messages.join(', ')
