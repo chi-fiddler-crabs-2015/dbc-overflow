dabce544a6f8b006f909d5c8d23690f2168b66f3
Ty Kowalewski <tykowale@gmail.com>
2015-03-20 15:41:51 -0500

    get it working

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,13 +3,14 @@ class VotesController < ApplicationController
   def create
     parent.votes.create(value: params[:vote_value], user: current_user)
     @vote = parent.vote_count
-    respond_to do |format|
-      format.js do
-          render partial: 'add_ajax_vote' and return
-        end
-      format.any do
-      end
-    end
+    # respond_to do |format|
+    #   format.js do
+    #       render partial: 'add_ajax_vote' and return
+    #     end
+    #   format.any do
+    #   end
+    # end
+    redirect_to question_path(params[:question_id])
   end
 
   private
