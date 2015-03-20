0872307000d98d541d5c5de35fa95358b01a9be7
Ty Kowalewski <tykowale@gmail.com>
2015-03-20 12:11:29 -0500

    working ajax for adding a new comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,6 +9,17 @@ class CommentsController < ApplicationController
     @comment.update_attributes(user: current_user)
 
     if @comment.save
+      respond_to do |format|
+        format.js do
+          if parent_string == "answer"
+            render partial: 'add_new_ajax_to_a' and return
+          else
+            render partial: 'add_new_ajax_to_q' and return
+          end
+        end
+        format.any do
+        end
+      end
       redirect_to question_path(params[:question_id])
     else
       @errors = @comment.errors.full_messages.join(', ')
@@ -22,6 +33,12 @@ class CommentsController < ApplicationController
     answer = Answer.find_by(id: params[:answer_id])
     return answer if answer
     Question.find_by(id: params[:question_id])
+  end
+
+  def parent_string
+    answer = Answer.find_by(id: params[:answer_id])
+    return "answer" if answer
+    "question"
   end
 
   def comment_params
