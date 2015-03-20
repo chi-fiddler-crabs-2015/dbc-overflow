e1ffaaaddc8fb2f844b0927a1bc453787ed1f9f2
Tyler Kowalewski <tykowale@gmail.com>
2015-03-20 07:02:38 -0500

    Add tests for comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:question) { create(:question) }
+  let(:user) {create(:user, username: 'Tyaasdfasdf', email: 'ty@example.com')}
+  before do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+  end
+
+  it { should belong_to(:user) }
+
+  it { should belong_to(:response) }
+
+  it 'throws an error if content is nil' do
+    comment = Comment.create(content: '')
+    expect(comment.errors).to_not be_nil
+  end
 end
