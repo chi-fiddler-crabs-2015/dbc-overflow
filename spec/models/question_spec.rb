5ffe08bf76ec31b58487576e05fb2c77ec24ad4e
Tyler Kowalewski <tykowale@gmail.com>
2015-03-20 06:37:34 -0500

    Start test for questions model

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:answers) }
+
+  it { should have_many(:comments) }
+
+  it { should have_many(:votes) }
+
+  it { should belong_to(:user) }
 end
