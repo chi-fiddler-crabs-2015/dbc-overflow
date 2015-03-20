7e7553548ac4c90d36de78bfe3e5709d6bf95cb4
Tyler Kowalewski <tykowale@gmail.com>
2015-03-20 07:03:24 -0500

    Remove unnecessary lets from comment model test

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,11 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  let(:question) { create(:question) }
-  let(:user) {create(:user, username: 'Tyaasdfasdf', email: 'ty@example.com')}
-  before do
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-  end
 
   it { should belong_to(:user) }
 
