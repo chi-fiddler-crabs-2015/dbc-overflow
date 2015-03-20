5f499d448799e2d7f0e2b645db442b0df26ed1d3
Tyler Kowalewski <tykowale@gmail.com>
2015-03-19 21:50:04 -0500

    first create route test

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -3,17 +3,20 @@ require 'rails_helper'
 
 describe AnswersController do
   let(:question) { create(:question) }
+  let(:user) {create(:user, username: 'Tyaasdfasdf', email: 'ty@example.com')}
+  before do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+  end
 
-  # context "POST #create" do
 
-  #   describe "when valid params are passed" do
-  #     # let(:new_question) { Question.create(title: "hey its a test", content: "hey its a content") }
+  context "POST #create" do
 
-  #     it 'creates a new questions' do
-  #       expect {
-  #         post :create, { question: {title: "hey its a test", content: "hey its a content"} }
-  #       }.to change{Question.count}.by(1)
-  #     end
+    describe "when valid params are passed" do
+      it 'creates a new answer' do
+        expect {
+          post :create, question_id: question.id, answer: {content: "hey its a content", user: user}}
+          .to change{Answer.count}.by(1)
+      end
 
   #     it 'assigns a newly created question as a question' do
   #       post :create, { question: {title: "hey its a test", content: "hey its a content"} }
@@ -37,7 +40,7 @@ describe AnswersController do
   #     it 'sends the user to new question page' do
 
   #     end
-  #   end
-  # end
+    end
+  end
 
 end
