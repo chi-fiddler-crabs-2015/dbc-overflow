2fa02bbb65fccf4596289a2078f852b80b573291
kaylindenberg <kay.lindenberg@gmail.com>
2015-03-20 21:11:18 -0500

    added respond_to line

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  respond_to :html, :js
+  
   def current_user
     @current_user ||= session[:user_id] && User.find_by(id: session[:user_id])
   end
